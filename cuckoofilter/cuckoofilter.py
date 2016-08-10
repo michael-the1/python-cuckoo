8b529e942025d7a314292e9c31ed6fb41d6a5aa0
Michael The <mcgthe@gmail.com>
2016-08-10 22:40:45 +0000

    Expand docstrings for CuckooFilter.

--- cuckoofilter/cuckoofilter.py
+++ cuckoofilter/cuckoofilter.py
@@ -4,17 +4,21 @@ import bitstring
 
 class CuckooFilter:
     '''
-    A Cuckoo Filter is data structure for probablistic set-membership queries.
-    With a very low false positive probability (FPP), we can ask whether it contains an item or not.
-    Cuckoo Filters serve as a drop-in replacement for Bloom filters, but are more space-efficient and support deletion of items.
+    A Cuckoo filter is a data structure for probablistic set-membership queries.
+    We can insert items into the filter and, with a very low false positive probability (FPP), ask whether it contains an item or not.
+    Cuckoo filters serve as a drop-in replacement for Bloom filters, but are more space-efficient and support deletion of items.
 
-    Cuckoo Filters were originally described in a paper by B. Fan, D. Andersen, M. Kaminsky, and M. Mitzenmacher (2014).
+    Cuckoo filters were originally described in:
+        Fan, B., Andersen, D. G., Kaminsky, M., & Mitzenmacher, M. D. (2014, December).
+        Cuckoo filter: Practically better than bloom.
+        In Proceedings of the 10th ACM International on Conference on emerging Networking Experiments and Technologies (pp. 75-88). ACM.
+    
     Their reference implementation in C++ can be found here: https://github.com/efficient/cuckoofilter
     '''
 
     def __init__(self, capacity, bits_per_fingerprint, bucket_size=4, max_kicks=500):
         '''
-        Initialize Cuckoo Filter parameters.
+        Initialize Cuckoo filter parameters.
 
         capacity : size of the filter
             Defines how many buckets the filter contains.
@@ -22,12 +26,10 @@ class CuckooFilter:
             Defines the size of the fingerprint.
             A larger fingerprint size results in a lower FPP.
             Using 6 bits results in over ~95% load factor, i.e., empirically it is found that the filter is filled over 95% before insertion failure occurs.
-
         bucket_size : nr. of entries a bucket can hold
             A bucket can hold multiple entries.
             Default size is 4, which closely approaches the best size for FPP between 0.00001 and 0.002 (see Fan et al.).
             If your targeted FPP is greater than 0.002, a bucket size of 2 is more space efficient.
-
         max_kicks : nr. of times entries are kicked around before deciding the filter is full
             Defaults to 500. This is an arbitrary number also used by Fan et al. and seems reasonable enough.
         '''
@@ -41,7 +43,7 @@ class CuckooFilter:
         '''
         Inserts an integer into the filter.
 
-        Throws an exception if the insertion failed.
+        Throws an exception if the insertion fails.
         '''
 
     def contains(self, item):
