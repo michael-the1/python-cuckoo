dabd45fdd81334d5e40d6efbcc2d66717b4d73b6
Michael The <mcgthe@gmail.com>
2016-08-22 18:44:52 +0200

    Add example usage script, modeled after the example script in the reference implementation.

---
+++ example.py
@@ -0,0 +1,26 @@
+'''
+Example usage. Modeled after https://github.com/efficient/cuckoofilter/blob/master/example/test.cc
+'''
+
+import cuckoofilter
+
+if __name__ == '__main__':
+    total_items = 1000000
+    cf = cuckoofilter.CuckooFilter(total_items, 2)
+    
+    num_inserted = 0
+    for i in range(total_items):
+        cf.insert(str(i))
+        num_inserted = num_inserted + 1
+
+    for i in range(num_inserted):
+        assert cf.contains(str(i))
+
+    total_queries = 0
+    false_queries = 0
+    for i in range(total_items, 2 * total_items):
+        if cf.contains(str(i)):
+            false_queries = false_queries + 1
+        total_queries = total_queries + 1
+
+    print('False positive rate is {}'.format(100.0 * false_queries / total_queries))
