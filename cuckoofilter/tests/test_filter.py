babdd3b92d7e03cc5412f9a99f620383dfd573e3
Michael The <mcgthe@gmail.com>
2016-08-25 16:17:17 +0200

    Cover CuckooFilter branches in insert and delete.

--- cuckoofilter/tests/test_filter.py
+++ cuckoofilter/tests/test_filter.py
@@ -6,13 +6,15 @@ def cf():
     return cuckoofilter.CuckooFilter(1000, 4)
 
 def test_insert(cf):
-    cf.insert('hello')
+    assert cf.insert('hello')
     assert cf.size == 1
 
 def test_insert_second_position(cf):
-    for _ in range(cf.bucket_size):
+    for _ in range(cf.bucket_size - 1):
         cf.insert('hello')
-    cf.insert('hello')
+    i1 = cf.insert('hello')
+    i2 = cf.insert('hello')
+    assert i1 != i2
 
 def test_insert_full(cf):
     # A cuckoofilter can hold at most 2 * bucket_size of the same fingerprint
@@ -21,6 +23,7 @@ def test_insert_full(cf):
 
     with pytest.raises(Exception) as e:
         cf.insert('hello')
+
     assert str(e.value) == 'Filter is full'
     assert cf.size == (cf.bucket_size * 2)
 
@@ -41,9 +44,19 @@ def test_contains_builtin(cf):
 
 def test_delete(cf):
     cf.insert('hello')
-    cf.delete('hello')
-    assert not cf.contains('hello'), 'Inserted key was not deleted.'
-    assert cf.size == 0, 'Size was not properly kept track of'
+    assert cf.delete('hello')
+    assert not cf.contains('hello')
+    assert cf.size == 0
+
+def test_delete_second_bucket(cf):
+    for _ in range(cf.bucket_size + 1):
+        cf.insert('hello')
+    for _ in range(cf.bucket_size + 1):
+        cf.delete('hello')
+    assert cf.size == 0
+
+def test_delete_non_existing(cf):
+    assert not cf.delete('hello')
 
 def test_load_factor_empty(cf):
     assert cf.load_factor() == 0
